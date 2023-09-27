0f9eb23bf8b2ff36faaf2fb9bd2ad7cb38483939
Isla <islarcherjr@gmail.com>
2023-09-27 05:58:14 +0100

    setup_done

---
+++ app.py
@@ -0,0 +1,19 @@
+# Steps
+"""
+- request for input
+- search for occurrences in source file
+- copy entire row of occurrence to new file
+- do this throughout the file
+- ask for input again
+"""
+import openpyxl
+import sys
+
+
+def main():
+    pattern = ""
+    source = openpyxl.load_workbook("")
+
+
+if __name__ == "__main__":
+    main()
