f5e0117fa14d6cd82c51fad2c3dbc9ccf272054d
Isla <islarcherjr@gmail.com>
2023-10-03 01:59:04 +0100

    Input stage done

--- app.py
+++ app.py
@@ -3,16 +3,51 @@
 - request for input
 - search for occurrences in source file
 - copy entire row of occurrence to new file
-- do this throughout the file
-- ask for input again
+- when done, ask for input again
+- repeat process on new sheet.
 """
 import openpyxl
 import sys
 
 
 def main():
-    pattern = ""
-    source = openpyxl.load_workbook("")
+    count = 0
+    while True:
+        while count >= 1:
+            re_prompt = input("Do you want to continue? (y or n) ")
+
+            re_prompt.strip().lower()
+
+            if re_prompt == "y" or re_prompt == "yes":
+                break
+
+            elif re_prompt == "n" or re_prompt == "no":
+                sys.exit()
+
+            else:
+                print("Provide a valid answer! ")
+                continue
+
+        pattern = input("What do you wish to sort? ")
+
+        # input validation
+        pattern.strip()
+
+        # load a workbook
+        source = openpyxl.load_workbook("./NLPC PFA.xlsx")
+        sheet = source.active
+
+        # saving to a file        
+        # source.save("test.csv")
+
+        # accessing one cell
+        # source.cell(row='', column='', value='')
+        # # or
+        # cell = source['A4'] = 4
+
+        # looping through cells
+
+        count = count + 1
 
 
 if __name__ == "__main__":
